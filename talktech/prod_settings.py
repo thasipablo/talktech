d91ce10f6bb161d91544fedd7041fb6ac518c277
Thasi Pablo <thasipablo@gmail.com>
2020-02-06 23:43:14 +0200

    configure postgres sql

--- talktech/prod_settings.py
+++ talktech/prod_settings.py
@@ -1,4 +1,5 @@
 from .settings import *
+import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = False
@@ -6,3 +7,5 @@ TEMPLATE_DEBUG = False
 SECRET_KEY = 't5rdh@nl4vgq*bw=88fut6n1rz2gih&r$5m@irl*go=g8#9xm7'
 
 ALLOWED_HOSTS = ['talktech.herokuapp.com']
+
+DATABASES['default'] = dj_database_url.config()
