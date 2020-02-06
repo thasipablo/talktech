d354252d77b5c57069db81f80f7ef95762150332
Thasi Pablo <thasipablo@gmail.com>
2020-02-07 00:09:39 +0200

    setup whitenoise module

--- talktech/prod_settings.py
+++ talktech/prod_settings.py
@@ -9,3 +9,6 @@ SECRET_KEY = 't5rdh@nl4vgq*bw=88fut6n1rz2gih&r$5m@irl*go=g8#9xm7'
 ALLOWED_HOSTS = ['talktech.herokuapp.com']
 
 DATABASES['default'] = dj_database_url.config()
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
